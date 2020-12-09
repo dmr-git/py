613ad9463be0a05424aadd4e4717c66518c484be
dmr-git <denrichter@gmail.com>
2020-12-09 17:55:42 -0500

    renamed library file

--- drutil_lib.py
+++ drutil_lib.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-# Filename:     drutils.py
+# Filename:     drutil_lib.py
 # Author:       DMR
 
 def sayhi(inName = 'Dennis'):
     ''' Prints a greeting.  Assumes inName is a str.'''
     print('Hi there,',inName)
 
-# testing section - can be commented out later
-
-sayhi()
+# testing section - can be uncommented later
+#sayhi()
