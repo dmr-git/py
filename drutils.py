4161af19bff7bda1b9dc7b1afb50374fd63b042f
dmr-git <denrichter@gmail.com>
2020-12-09 06:10:34 -0500

    initial files added

---
+++ drutils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Filename:     drutils.py
+# Author:       DMR
+
+def sayhi(inName = 'Dennis'):
+    ''' Prints a greeting.  Assumes inName is a str.'''
+    print('Hi there,',inName)
+
+# testing section - can be commented out later
+
+sayhi()
---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import os
+
+os.system("clear")
+print('Hello '+ os.environ['USER'] + '!\n')
+
+
