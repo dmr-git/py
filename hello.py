bd4e6d352bd3900636b678380365521205b8fc66
dmr-git <denrichter@gmail.com>
2020-12-10 18:52:58 -0500

    added functionality for reading command line arguments

--- hello.py
+++ hello.py
@@ -3,8 +3,21 @@
 # Author: DMR
 
 import os
+import sys
 
-os.system("clear")
-print('Hello '+ os.environ['USER'] + '!!!\n')
+def cat():
+    print('Meow')
 
+def default():
+    print('Hello '+ os.environ['USER'] + '!\n')
+
+def main():
+    os.system("clear")
+    if len(sys.argv) == 2 and sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
+
+if __name__ == '__main__':
+    main()
 
