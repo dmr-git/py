d1da5701bf9d3c75fd5c5f142a5f760ac6d53b1e
dmr-git <denrichter@gmail.com>
2022-03-01 08:19:38 -0500

    minor changes

---
+++ rpi/b2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from gpiozero import Buzzer
+from time import sleep
+
+buzzer = Buzzer(19)
+
+while True:
+#    buzzer.on()
+#    sleep(.1)
+#    buzzer.off()
+#    sleep(1)
+    buzzer.beep(.1,1,None,False)
