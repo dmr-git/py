86c6cd0642b3db43b77d06dd25d33fc38e1e7273
dmr-git <denrichter@gmail.com>
2022-04-29 07:27:53 -0400

    more changes

---
+++ bin_count.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Author: DMR
+"""
+Set up 5 LEDs as outputs on different GPIO pins
+"""
+
+import RPi.GPIO as GPIO
+from os import system
+from time import sleep
+
+
+def light_up(x, led):
+    x_bin_lst = list(str(bin(x)[2:].zfill(5)))
+
+    for bulb in range(5):
+        if x_bin_lst[bulb] == '1':
+            GPIO.output(led[bulb], True)
+        else:
+            GPIO.output(led[bulb], False)
+
+def main():
+    system("clear")
+    GPIO.setmode(GPIO.BCM)
+
+    led = {0:25, 1:24, 2:23, 3:26, 4:6}
+
+    # set the pins to Output
+    for i in range(0,len(led)):
+        GPIO.setup(led.get(i), GPIO.OUT)
+    
+    value = 0
+
+    for i in range(32):
+        light_up(i,led)
+        sleep(1)
+    GPIO.cleanup() 
+
+if __name__ == '__main__':
+    main()
+
