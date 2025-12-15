9e5dd5248c023dda7e71e75ae3616ec005c7238a
dmr-git <denrichter@gmail.com>
2025-12-15 12:44:57 -0500

    Added files

--- hardway/ex4.py
+++ hardway/ex4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 cars = 100
-space_in_a_car = 4
+space_in_a_car = 4.0
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
---
+++ hardway/ex7.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+print("Mary had a little lamb.")
+print("Its fleece was white as {} {}".format('snow', 'birds'))
+print("And everywhere that Mary went.")
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+# This code prints a short poem and then constructs and prints the words "Cheese Burger" by concatenating individual characters.
